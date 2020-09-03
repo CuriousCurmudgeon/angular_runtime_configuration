26367cd0a29eba7d01b639f6175258490f66604c
Brian Meeker <meeker.brian@gmail.com>
2020-09-02 22:22:21 -0400

    Load the config file during runtime before Angular bootstraps.
    * We have to do this before Angular bootstraps in order to call enableProdMode().
    * We save the config file contents on the window object so that we can use it later during Angular initialization to create a strongly typed config class.

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,20 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-// if (environment.production) {
-//   enableProdMode();
-// }
+fetch('/assets/config/environment.json')
+  .then(response => response.json())
+  .then(config => {
+    // We use this syntax to appease Typescript.
+    // __env does not exist on window. We're adding it.
+    window['__env'] = window['__env'] || {};
+    window['__env'].config = config;
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+    if (config.production) {
+      enableProdMode();
+    }
+
+    platformBrowserDynamic().bootstrapModule(AppModule)
+      .catch(err => console.error(err));
+  });
+
+
